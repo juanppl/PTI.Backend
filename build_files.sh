53822e31ad34e7d0df24c2365fa2794768904431
juanppl <jppl505@hotmail.com>
2024-08-28 20:18:11 -0500

    remove build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
-python3.12 -m venv venv
-source venv/bin/activate
+# python3.12 -m venv venv
+# source venv/bin/activate
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
